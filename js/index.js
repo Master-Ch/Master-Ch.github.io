76bc74d388cd3c3387821974151901c375e99299
Master-Ch <2240321004@qq.com>
2020-05-07 13:41:55 +0800

    添加时间

--- js/index.js
+++ js/index.js
@@ -181,3 +181,7 @@ function closeTool() {
     },1000);
     
 }
+
+// 显示时间
+//document.getElementById('time').innerHTML = new Date().toLocaleString()+ ' 星期' + '日一二三四五六'.charAt(new Date().getDay());
+setInterval("document.getElementById('time').innerHTML=new Date().toLocaleString()+'&nbsp'+' 星期'+'日一二三四五六'.charAt(new Date().getDay());",1000);
